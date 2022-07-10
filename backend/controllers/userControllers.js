07ab9a15767348aefd812f95acbbef1f16e2b177
Navaneeth Devan <navaneethdevanofficial@gmail.com>
2022-07-10 21:13:12 +0530

    deployment

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -54,17 +54,6 @@ const authUser= asyncHandler(async (req,res) => {
 
     const user =await User.findOne({ email });
 
-    // if(user &&  (await user.matchPassword(password))) {
-    //     res.json({
-    //         _id:user._id,
-    //         name:user.name,
-    //         email:user.email,
-    //         token:generateToken(user._id)
-    //     })
-    // }else {
-    //     res.status (400)
-    //     throw new Error ("Invalid Email or Psssword !" )
-    // }
     if (user) {
         let notMatch = await user.matchPassword(password);
         if (notMatch) {
